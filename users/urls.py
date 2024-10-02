ec11e9528a1213f6717adfde377761092801d76b
Karar <krarhayder45@gmail.com>
2024-10-02 04:02:45 +0300

    made a login page

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
-
+from django.urls import path
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    
+    path("users/login/",LoginView.as_view(template_name='users/login.html'),name='Login')
 ]
