5af4d083cfa7c1349a5592defa1245003f8d1456
Karar <krarhayder45@gmail.com>
2024-10-02 14:11:31 +0300

    Added pharmacy and supplier

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Pharmacy, Supplier
 # Register your models here.
+
+admin.site.register(Pharmacy)
+admin.site.register(Supplier)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    contact_info = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Pharmacy(models.Model):
+    name = models.CharField('Name',max_length=255)
+    owner = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    workers = models.ManyToManyField(User,related_name='pharmacies')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Pharmacy"
+        verbose_name_plural = "Pharmacies"
