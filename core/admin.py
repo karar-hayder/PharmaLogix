bd40a177a77bf8d80348bda0fb6c7302edc9b77c
Karar <krarhayder45@gmail.com>
2024-10-03 03:30:42 +0300

    Added Sale and SaleItem

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Medication, Product
+from .models import Medication, Product, Sale, SaleItem
 # Register your models here.
 
 admin.site.register(Medication)
 admin.site.register(Product)
+admin.site.register(Sale)
+admin.site.register(SaleItem)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import Pharmacy, Supplier
+from users.models import Pharmacy, Supplier, User
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 # Create your models here.
@@ -21,6 +21,8 @@ DOSAGE_FORMS = [
     ('granules', 'Granules'),
     ('ointment', 'Ointment'),
     ('cream', 'Cream'),
+    ('patch', 'Patch'),
+    ('enema', 'Enema'),
     ('gel', 'Gel'),
     ('liniment', 'Liniment'),
     ('suppository', 'Suppository'),
@@ -43,7 +45,7 @@ class Medication(models.Model):
     def __str__(self):
         return self.name
     class Meta:
-        unique_together = ('name','dosage_form','strength')
+        unique_together = ('name','manufacturer','dosage_form','strength')
     
 class Product(models.Model):
     pharmacy = models.ForeignKey(Pharmacy,on_delete=models.CASCADE)
@@ -64,3 +66,23 @@ class Product(models.Model):
         indexes = [
             models.Index(fields=['barcode']),
         ]
+
+class Sale(models.Model):
+    pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE)
+    cashier = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_amount = models.PositiveIntegerField()
+    discount = models.PositiveIntegerField()
+    
+
+    def __str__(self):
+        return f"Sale #{self.id} - {self.total_amount} by {self.cashier.username} at {self.created_at.strftime('%d/%m/%Y, %H:%M:%S')}"
+
+class SaleItem(models.Model):
+    sale = models.ForeignKey(Sale, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField()
+
+    def total_price(self):
+        return self.quantity * self.price
