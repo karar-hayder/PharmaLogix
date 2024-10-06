c0cc4fd687c330a5131910b7bf41f37097b734b4
Karar <krarhayder45@gmail.com>
2024-10-06 19:02:36 +0300

    Added BarcodeAdderView

--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
 from typing import Any
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Pharmacy
+from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Pharmacy, Medication
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from rest_framework import status
+from .serializers import MedicationSerializer
+import json
 # Create your views here.
 
 
@@ -28,3 +32,69 @@ class Work(LoginRequiredMixin,TemplateView):
         pharmacy = get_object_or_404(Pharmacy, id=pharmacy_id)
         context['pharmacy'] = pharmacy
         return context
+
+class BarcodeAdderView(LoginRequiredMixin,UserPassesTestMixin,TemplateView):
+    template_name = 'core/barcode_add.html'
+
+    def test_func(self) -> bool | None:
+        return self.request.user.is_superuser
+    def post(self, request, *args, **kwargs):
+        barcode = request.POST.get('barcode')
+
+        if not barcode:
+            return JsonResponse({"error": "Barcode is required"}, status=400)
+
+        try:
+            medication = Medication.objects.get(barcode=barcode)
+            data = MedicationSerializer(medication).data
+            return JsonResponse({
+                "message": "Medication found",
+                "medication": data
+            }, status=200)
+
+        except Medication.DoesNotExist:
+            return JsonResponse({
+                "message": "Medication not found. Please search or create a new one."
+            }, status=404)
+
+    def put(self, request, *args, **kwargs):
+
+        try:
+            put_data = json.loads(request.body.decode('utf-8'))
+            print(put_data)
+            medication_id = put_data.get('medication_id')
+            barcode = put_data.get('barcode')
+
+            if not medication_id or not barcode:
+                return JsonResponse({"error": "Medication ID and barcode are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+            try:
+                medication = Medication.objects.get(id=medication_id)
+            except Medication.DoesNotExist:
+                return JsonResponse({"error": "Medication not found."}, status=status.HTTP_404_NOT_FOUND)
+
+            medication.barcode = barcode
+
+            if 'name' in put_data:
+                medication.name = put_data['name']
+            if 'generic_name' in put_data:
+                medication.generic_name = put_data['generic_name']
+            if 'manufacturer' in put_data:
+                medication.manufacturer = put_data['manufacturer']
+            if 'dosage_form' in put_data:
+                medication.dosage_form = put_data['dosage_form']
+            if 'strength' in put_data:
+                medication.strength = put_data['strength']
+            if 'active_ingredients' in put_data:
+                medication.active_ingredients = put_data['active_ingredients']
+
+            medication.save()
+            data = MedicationSerializer(medication).data
+
+            return JsonResponse({
+                "message": "Medication successfully updated.",
+                "medication": data
+            }, status=status.HTTP_200_OK)
+
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON data."}, status=status.HTTP_400_BAD_REQUEST)
