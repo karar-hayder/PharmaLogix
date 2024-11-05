f6da5597b22f090859b0aced62fbb53543676a84
Karar <krarhayder45@gmail.com>
2024-11-05 20:34:07 +0300

    Added a link to staff pharmacies

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("users/login/",LoginView.as_view(template_name='users/login.html'),name='Login'),
-    path("staff/pharmacies/",views.PharmacyList.as_view())
+    path("staff/pharmacies/",views.PharmacyList.as_view(),name="Staff pharmacies list")
 ]
