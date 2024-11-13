c34a31eab0569fe6c8449476b4fd78df64c975c1
Karar <krarhayder45@gmail.com>
2024-11-13 13:55:12 +0300

    Made the plan subscribe view

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("users/login/",LoginView.as_view(template_name='users/login.html'),name='Login'),
-    path("staff/pharmacies/",views.PharmacyList.as_view(),name="Staff pharmacies list")
+    path("staff/pharmacies/",views.PharmacyList.as_view(),name="Staff pharmacies list"),
+    path("staff/pharmacies/subscribe/<int:pharmacy_id>/",views.ChoosePlanView.as_view(),name="Staff pharmacies subscribe")
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
-from django.views.generic import ListView, View
+from typing import Any
+from django.views.generic import ListView, View, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
+from django.utils import timezone
 from django.core.cache import cache
-
-from .models import Pharmacy
+from core.extras import hash_key
+from .models import Pharmacy, SubscriptionPlan, Subscription
 # Create your views here.
 
 class BaseStaffPage(LoginRequiredMixin,UserPassesTestMixin,View):
@@ -25,3 +29,32 @@ class PharmacyList(BaseStaffPage,ListView):
     context_object_name = 'pharmacies'
     ordering = '-created_at'
     paginate_by = 30
+
+
+class ChoosePlanView(BaseStaffPage,TemplateView):
+    template_name = 'users/choose_plan.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        cache_key = hash_key('plans')
+        plans = cache.get(cache_key)
+        if not plans:
+            plans = SubscriptionPlan.objects.all()
+            cache.set(cache_key,plans,60*60*6)
+        context['plans'] = plans
+        return context
+
+    def post(self, request, pharmacy_id, *args, **kwargs):
+        plan_id = request.POST.get('plan_id')
+        plan = get_object_or_404(SubscriptionPlan, id=plan_id, is_active=True)
+        pharmacy = get_object_or_404(Pharmacy, id=pharmacy_id)
+        subscription = Subscription.objects.create(
+            pharmacy=pharmacy,
+            plan=plan,
+            start_date=timezone.now().date(),
+            paid=plan.total_price
+        )
+        cache.delete(f"{pharmacy.pk}-sub")
+        messages.success(request, f"Subscription for {plan.name} has been successfully created for {pharmacy.name}.")
+    
+        return redirect('Staff pharmacies list')
